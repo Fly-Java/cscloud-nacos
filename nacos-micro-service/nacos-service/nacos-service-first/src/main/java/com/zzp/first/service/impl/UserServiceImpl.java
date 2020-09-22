6fb19c08313e26c856c8047544d22baed460716d
Java-Fly <1139160517@qq.com>
2020-09-22 14:50:07 +0800

    ''

--- nacos-micro-service/nacos-service/nacos-service-first/src/main/java/com/zzp/first/service/impl/UserServiceImpl.java
+++ nacos-micro-service/nacos-service/nacos-service-first/src/main/java/com/zzp/first/service/impl/UserServiceImpl.java
@@ -6,7 +6,9 @@ import com.zzp.common.entity.User;
 import com.zzp.common.utils.SnowflakeIdWorker;
 import com.zzp.first.mapper.UserMapper;
 import com.zzp.first.service.UserService;
+import com.zzp.first.utils.RedisServiceUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -18,6 +20,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     @Resource
     private UserMapper userMapper;
+    @Autowired
+    private RedisServiceUtil redisServiceUtil;
 
     @Override
     public String consumerService() {
@@ -28,6 +32,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     public List<User> getUser() {
         List<User> userList = userMapper.selectList(new QueryWrapper<User>().lambda().eq(User::getName, "s"));
         log.info("userList:" + userList);
+        redisServiceUtil.set("jia3", "xin");
         return userList;
     }
 
